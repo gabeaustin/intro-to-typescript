9eb4d4ef6e10e220af51520390facba9c491068e
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-10 19:17:22 -0600

    updated code for objects and arrays

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,30 @@
-var character = "mario";
-var age = 30;
-var isBlackBelt = false;
-// character = 20;
-character = "luigi";
-// age = "yosi";
-age = 40;
-// isBlackBelt = "yes";
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+// names.push(3);
+// names[0] = false;
+var numbers = [10, 20, 30, 40];
+// numbers.push(25);
+// numbers.push("shawn");
+var mixed = ["ken", 4, "chun-li", 8, 9, true];
+mixed.push("ryu");
+mixed.push(8);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30
 };
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = "ryu";
+// ninja.age = "30";
+// ninja.skills = ["fighting"];
+// override entire object
+// must match structure that we initially declared
+ninja = {
+    name: "yoshi",
+    belt: "yellow",
+    age: 40
+    // skills: []
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,37 @@
-let character = "mario";
-let age: number = 30;
-let isBlackBelt = false;
+// arrays
+let names = ["luigi", "mario", "yoshi"];
 
-// character = 20;
-character = "luigi";
+names.push("toad");
+// names.push(3);
+// names[0] = false;
 
-// age = "yosi";
-age = 40;
+let numbers = [10, 20, 30, 40];
+// numbers.push(25);
+// numbers.push("shawn");
 
-// isBlackBelt = "yes";
-isBlackBelt = true;
+let mixed = ["ken", 4, "chun-li", 8, 9, true];
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI
+mixed.push("ryu");
+mixed.push(8);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30
+};
+
+ninja.age = 40;
+ninja.name = "ryu";
+// ninja.age = "30";
+// ninja.skills = ["fighting"];
+
+// override entire object
+// must match structure that we initially declared
+ninja = {
+    name: "yoshi",
+    belt: "yellow",
+    age: 40
+    // skills: []
 }
-
-console.log(circ(7.5));
